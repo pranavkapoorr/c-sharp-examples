d425a120d31d626db4112940b06e28bf943d02fb
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-03-22 21:42:57 -0400

    arrays added

---
+++ arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arrays{
+    class Program{
+        static String[] myArray = {"element-0","element-1","element-2","element-3","element-4","element-5","element-6","element-7","element-8","element-9"};
+        static void Main(string[] args){
+            foreach (var item in myArray){
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
