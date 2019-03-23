2f571d3a5192a53d38c4da9c719cfa2c13cd70be
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-03-23 13:14:59 -0400

    inheritance

---
+++ inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace inheritance {
+    class Shape {
+        protected int length, width;
+        public void setLength(int length){
+            this.length = length;
+        }
+        public void setWidth(int width){
+            this.width = width;
+        }
+    }
+    class Square : Shape{
+        public int Area(){
+            return (length * width);
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            Square sq1 = new Square();
+            sq1.setLength(5);
+            sq1.setWidth(5);
+            Console.WriteLine("area of square: {0}",sq1.Area());
+        }
+    }
+}
